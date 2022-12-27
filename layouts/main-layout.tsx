d6c6852c5fe896c62d963d4d42ab375a3d8581f7
Ryan10145 <chang.ryan10145@gmail.com>
2022-12-27 18:43:09 -0500

    Add support for more blog rendering features and change font

--- layouts/main-layout.tsx
+++ layouts/main-layout.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react'
 import { Box, Container } from '@chakra-ui/react'
-import Head from 'next/head'
 import { BasicProps } from 'lib/react-utils'
 import Navbar from 'components/navbar'
 import Footer from 'components/footer'
