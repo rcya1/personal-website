5c7022dcac53a4b2d2c72076f47c9c17f88b4c53
Ryan10145 <chang.ryan10145@gmail.com>
2022-12-28 15:53:35 -0500

    Fix flickering on page transition and add custom scrollbar

--- layouts/main-layout.tsx
+++ layouts/main-layout.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react'
 import { Box, Container } from '@chakra-ui/react'
-import { BasicProps } from 'lib/react-utils'
+import useWindowDimensions, { BasicProps } from 'lib/react-utils'
 import Navbar from 'components/navbar'
 import Footer from 'components/footer'
 import { ChakraAnimate } from 'lib/animate'
+import Scrollbars from 'react-custom-scrollbars'
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
@@ -12,26 +13,37 @@ const variants = {
 }
 
 const MainLayout: FC<BasicProps> = ({ children }) => {
+  const { width, height } = useWindowDimensions()
+
   return (
-    <Box as="main" pb={8}>
-      <Navbar />
+    <Scrollbars
+      universal={true}
+      autoHide
+      autoHideTimeout={1000}
+      autoHideDuration={200}
+      autoHeight
+      autoHeightMax={height}
+    >
+      <Box as="main" pb={8}>
+        <Navbar />
 
-      <Container maxW="container.md" mt={16}>
-        <ChakraAnimate
-          initial="hidden"
-          animate="enter"
-          exit="exit"
-          variants={variants}
-          // @ts-ignore
-          transition={{ duration: 0.4 }}
-          style={{ position: 'relative' }}
-        >
-          {children}
-        </ChakraAnimate>
-      </Container>
+        <Container maxW="container.md" mt={16}>
+          <ChakraAnimate
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            // @ts-ignore
+            transition={{ duration: 0.4 }}
+            style={{ position: 'relative' }}
+          >
+            {children}
+          </ChakraAnimate>
+        </Container>
 
-      <Footer />
-    </Box>
+        <Footer />
+      </Box>
+    </Scrollbars>
   )
 }
 
